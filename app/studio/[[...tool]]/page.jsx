49a63a5d7d051d260d288593ce44b6b6be3474d1
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-15 20:29:51 +0530

    Bug fix

--- app/studio/[[...tool]]/page.jsx
+++ app/studio/[[...tool]]/page.jsx
@@ -10,7 +10,7 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
