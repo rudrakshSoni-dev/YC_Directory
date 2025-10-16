0d10d57191c1cfe7a91a40f401f4b1687913c563
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-16 16:45:20 +0530

    Added new route

---
+++ app/patches/fix-html-context.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const HtmlContext = React.createContext({});
+export default HtmlContext;
--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,39 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import path from "path";
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-    turbo: false,
-    // ppr: 'incremental'//changed it 
-    // cacheComponents:'incremental',
-     cacheComponents: true,
-     after: true,
+  experimental: {
+    turbo: false, // disables turbopack
+    cacheComponents: true,
+    after: true,
   },
-    devIndicators:{
-      appIsrStatus: true,
-      buildActivity: true,
-      buildActivityPosition: 'bottom-right',
-    },
-    images: {
-      dangerouslyAllowSVG: true,
+
+  webpack: (config) => {
+    // 🧩 Fix for missing html-context in Next.js 15
+    config.resolve.alias[
+      "next/dist/server/route-modules/app-page/vendored/contexts/html-context"
+    ] = path.resolve("./patches/fix-html-context.js");
+
+    return config;
+  },
+
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
+
+  images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
+        protocol: "https",
+        hostname: "plus.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'placehold.co', // also allow your fallback image
+        protocol: "https",
+        hostname: "placehold.co",
       },
     ],
     domains: ["avatars.githubusercontent.com"],
@@ -30,34 +41,10 @@ const nextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "none-fqi",
-
   project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 });
